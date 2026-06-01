ab20eb22f44d8362752df6a4c20c340b180da364
Lopz532 <jorgelopz532@gmail.com>
2026-06-01 12:17:09 -0600

    modified:   .gitignore 	modified:   public/test.php

--- public/test.php
+++ public/test.php
@@ -1,6 +1,6 @@
 <?php
 
-$url = "https://script.google.com/macros/s/AKfycbwN9kBAK6aSRTU7v3XnSwmizmzHg2t1i3JIiSqYgbsqQuXTci7CFIl5jKCUTXj2OIbffA/execv";
+$url = "https://script.google.com/macros/s/AKfycbwN9kBAK6aSRTU7v3XnSwmizmzHg2t1i3JIiSqYgbsqQuXTci7CFIl5jKCUTXj2OIbffA/exec";
 
 $data = [
     "nombre" => "JVLEX",
@@ -9,15 +9,27 @@ $data = [
 
 $ch = curl_init($url);
 
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    "Content-Type: application/json"
+curl_setopt_array($ch, [
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => json_encode($data),
+    CURLOPT_HTTPHEADER => [
+        "Content-Type: application/json"
+    ],
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_TIMEOUT => 30
 ]);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $response = curl_exec($ch);
+$error = curl_error($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
 curl_close($ch);
 
-echo $response;
+echo "<h2>Resultado</h2>";
+
+if ($error) {
+    echo "<p>Error cURL: $error</p>";
+} else {
+    echo "<p>HTTP Code: $httpCode</p>";
+    echo "<pre>$response</pre>";
+}
