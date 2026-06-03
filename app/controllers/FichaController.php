ac94df6712f1b270037ea4da8d18bd97af38ddfe
Lopz532 <jorgelopz532@gmail.com>
2026-06-03 01:42:18 -0600

    modified:   app/controllers/FichaController.php

--- app/controllers/FichaController.php
+++ app/controllers/FichaController.php
@@ -14,6 +14,7 @@ class FichaController
 
     public function registrar(array $request = []): void
     {
+        error_log('REQUEST:' .json_encode($request));
         $errors = FichaValidator::validate($request);
 
         if (!empty($errors)) {
